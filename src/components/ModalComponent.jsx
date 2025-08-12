ab1c9fbf721343c3d9b2e4b2e671fd9ee6994f82
John Patrick <jpadelasalas@gmail.com>
2025-08-12 10:46:47 +0800

    global modal component

---
+++ src/components/ModalComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+
+const ModalComponent = React.memo(
+  ({ title, handleCloseModal, children, footer }) => {
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-white/30">
+        {/* Modal Box */}
+        <div className="bg-white rounded-lg shadow-lg w-11/12 max-w-md text-sm md:text-md ">
+          <header className="text-sm md:text-md lg:text-xl bg-violet-100 flex justify-between p-4 rounded-t-lg">
+            <span className="font-semibold ">{title}</span>
+            <CloseIcon onClick={handleCloseModal} className="cursor-pointer" />
+          </header>
+          {children}
+          {footer}
+        </div>
+      </div>
+    );
+  }
+);
+
+export default ModalComponent;
