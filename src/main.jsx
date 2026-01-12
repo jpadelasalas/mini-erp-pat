68047e0aaf15a1d445bf19b26056277a421a4b04
John Patrick <jpadelasalas@gmail.com>
2026-01-12 13:50:53 +0800

    remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </StrictMode>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 );
